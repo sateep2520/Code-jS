ab0e9dc408d7414798dd855a65b966d79b279fcd
Sateep Mohanty <98683263+sateep2520@users.noreply.github.com>
2025-07-27 04:38:25 +0000

    assss

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -1,1 +1,31 @@
-console.log("Sateep");
+//console.log("Sateep");
+let score = 93
+let isPassed = false;
+
+//console.log(typeof score);
+//console.log(typeof isPassed);   
+
+let valueInisPassed = Number(isPassed);
+console.log(valueInisPassed); // This will log 0, as false is converted to 0
+console.log(typeof valueInisPassed);
+
+ // let valueInisPassed=number(isPassed);This will throw an error because'number'is not a valid function in JavaScript
+
+ // Declaring symbols
+ const id = Symbol("2005");
+ const id2 = Symbol("2005");
+    console.log(id === id2); // This will log false, as each Symbol is unique even if they have the same description
+
+// Using BigInt
+ const bigIntValue = BigInt(1234567890123456789012345678901234567890);
+// or we can use the BigInt literal syntax
+// const bigIntValue = 1234567890123456789012345678901234567890n;( This will also create a BigInt)
+console.log(typeof bigIntValue); // This will log "bigint"
+console.log(bigIntValue); // This will log the BigInt value
+
+// Using null and undefined
+let value = null; // This is explicitly set the value to null
+let notAssigned; // This is undefined because it has not been assigned a value
+
+//console.log(value); // This will log null
+//console.log(notAssigned); // This will log undefined
